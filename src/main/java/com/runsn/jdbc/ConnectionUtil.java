1c6bf55a6c55b4fa9e9ab4c70e5bd69d8b63e14f
xieguoqiang <xieguoqiang@chinadrtv.com>
2013-10-27 00:05:28 +0800

    数据库相关配置抽取到配置文件

--- src/main/java/com/runsn/jdbc/ConnectionUtil.java
+++ src/main/java/com/runsn/jdbc/ConnectionUtil.java
@@ -1,5 +1,8 @@
 package com.runsn.jdbc;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -12,17 +15,54 @@ import java.sql.DriverManager;
  * @version 1.0
  */
 public class ConnectionUtil {
+    private static String driverClassName;
+    private static String url;
+    private static String username;
+    private static String password;
+
     /* 获取数据库连接的函数*/
     public static Connection getConnection() {
         Connection con = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName(driverClassName);
             con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/runsn", "root", "root");
+                    url, username, password);
 
         } catch (Exception e) {
             System.out.println("数据库连接失败。" + e.getMessage());
         }
         return con;
+    }
+
+    public static String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public static void setDriverClassName(String driverClassName) {
+        ConnectionUtil.driverClassName = driverClassName;
+    }
+
+    public static String getUrl() {
+        return url;
+    }
+
+    public static void setUrl(String url) {
+        ConnectionUtil.url = url;
+    }
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        ConnectionUtil.username = username;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String password) {
+        ConnectionUtil.password = password;
     }
 }
